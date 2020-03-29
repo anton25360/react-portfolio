fd8046ed05a1a605d7037ba07bca222bd52d9561
Anton Webb <anton25360@gmail.com>
2020-03-29 16:01:25 +0100

    Added landing to App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 //components
 import Navbar from './components/Navbar'
+import Landing from './components/Landing'
 
 function App() {
   return (
     <div >
       <Navbar />
+      <Landing />
       
     </div>
   );
