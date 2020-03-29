4dde26deb4c7807fdae12a5bc37d7da1a1817f54
Anton Webb <anton25360@gmail.com>
2020-03-29 17:41:55 +0100

    Changed text color

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export class About extends Component {
         return (
             <div className='aboutContainer'>
                 <p className='aboutTitle'>About Me</p>
-                <p className=' aboutText'>I am a bilingual (French & English) Full Stack Web Developer from Bath, UK. Thanks to my time at Mayden Academy (as opposed to University) I have experience with many different front and back end languages, and an exeptional eye for well-written code and clean UIs. <br/><br/> I always enjoy putting my skills to the test with various different side-projects (Which you can see below), as well as independantly learn new languages (Such as Java and Go).<br/><br/>When I'm not programming, you'll find me walking my dog, tinkering with one of my many Raspberry Pi's, or reading a good Fantasy novel.</p>
+                <p className=' aboutText'>I am a bilingual (French & English) Full Stack Web Developer from Bath, UK. Thanks to my time at Mayden Academy (as opposed to University) I have experience with many different front and back end languages, and an exeptional eye for well-written code and clean UIs. <br /><br /> I always enjoy putting my skills to the test with various different side-projects (Which you can see below), as well as independantly learn new languages (Such as Java and Go).<br /><br />When I'm not programming, you'll find me walking my dog, tinkering with one of my many Raspberry Pi's, or reading a good Fantasy novel.</p>
             </div>
         )
     }
