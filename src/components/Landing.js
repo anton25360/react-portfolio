1b1dcc092e55ebad13da221f9914d6be76f5105d
Anton Webb <anton25360@gmail.com>
2020-03-29 16:22:08 +0100

    Adde 'view my work' button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,8 @@ export class Landing extends Component {
             <div className='landingContainer'>
                 <div className='landingTextContainer'>
                     <h1 className='landingTextGreeting'>Hi, I'm Anton</h1>
-                    <p className='landingTextSub'>Full Stack Web Developer</p>
+                    <p className='landingTextSub'>Full Stack Web Developer</p><br/><br/>
+                    <button className='landingTextBtn'>View My Work</button>
                 </div>
             </div>
         )
