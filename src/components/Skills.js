09eaf1e7a32dbea28b07948077106d698336170b
Anton Webb <anton25360@gmail.com>
2020-03-29 20:57:11 +0100

    Skills now display on 2 lines

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,22 +11,24 @@ export class Skills extends Component {
                     <p className='badgeFormat badge-css'>CSS</p>
                     <p className='badgeFormat badge-sass'>Sass</p>
                     <p className='badgeFormat badge-bootstrap'>Bootstrap</p>
-                    <br/>
+                    {/* <br/> */}
                     <p className='badgeFormat badge-php'>PHP</p>
                     <p className='badgeFormat badge-js'>JavaScript</p>
                     <p className='badgeFormat badge-ts'>TypeScript</p>
+                    <p className='badgeFormat badge-git'>git</p>
+
                     <br/>
                     <p className='badgeFormat badge-java'>Java</p>
                     <p className='badgeFormat badge-firebase'>Google Firebase</p>
                     <p className='badgeFormat badge-xml'>XML</p>
-                    <br/>
+                    {/* <br/> */}
                     <p className='badgeFormat badge-react'>ReactJS</p>
                     <p className='badgeFormat badge-pwa'>PWA</p>
                     <p className='badgeFormat badge-node'>NodeJS</p>
-                    <br/>
+                    {/* <br/> */}
                     <p className='badgeFormat badge-go'>Go</p>
                     <p className='badgeFormat badge-python'>Python</p>
-                    <p className='badgeFormat badge-git'>git</p>
+                    {/* <p className='badgeFormat badge-git'>git</p> */}
                 </div>
             </div>
         )
