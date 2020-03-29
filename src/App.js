aa424b5dcfaf5fcb22c5e055a35b050a6b27cd40
Anton Webb <anton25360@gmail.com>
2020-03-29 17:23:58 +0100

    added about me component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 //components
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
+import About from './components/About'
 
 function App() {
   return (
     <div >
       <Navbar />
       <Landing />
+      <About />
       
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './About.css'
+
+export class About extends Component {
+    render() {
+        return (
+            <div className='aboutContainer'>
+                <p className='aboutTitle'>About Me</p>
+                <p className=' aboutText'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
+            </div>
+        )
+    }
+}
+
+export default About
