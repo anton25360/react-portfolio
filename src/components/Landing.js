40f35596aa2f7a1eff257c2bc6a6e4a75066cf45
Anton Webb <anton25360@gmail.com>
2020-03-30 17:38:18 +0100

    Added new projTarget for buttons

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -16,7 +16,7 @@ export class Landing extends Component {
                 <div className='landingTextContainer'>
                     <h1 className='landingTextGreeting'>Hi, I'm Anton.</h1>
                     <p className='landingTextSub'>I'm a Full Stack Web Developer looking for a new challenge.</p><br/><br/>
-                    <button className='landingTextBtn' onClick={()=> this.scroll('.projectContainer')}>View My Work</button>
+                    <button className='landingTextBtn' onClick={()=> this.scroll('.projTarget')}>View My Work</button>
                     <button className='landingTextBtn' onClick={()=> this.scroll('.contactContainer')}>Get In Touch</button>
                 </div>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,12 +7,15 @@ import ProjectTemplate from './ProjectTemplate'
 export class Projects extends Component {
     render() {
         return (
+            <div className='projTarget'>
+            <p id='projTitle'>Projects.</p><br/>
             <div className='projectContainer'>
                 <ProjectTemplate title='Wonders' description='7 Wonders of the World PWA.' icon='react'/>
                 <ProjectTemplate title='Wonders' description='7 Wonders of the World PWA.' icon='react'/>
                 <ProjectTemplate title='Wonders' description='7 Wonders of the World PWA.' icon='react'/>
                 <ProjectTemplate title='Wonders' description='7 Wonders of the World PWA.' icon='react'/>
                 <ProjectTemplate title='Wonders' description='7 Wonders of the World PWA.' icon='react'/>
+            </div>
             </div>
         )
     }
