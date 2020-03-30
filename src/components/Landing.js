81dbd1158209abfe0ac34f8adf032a340436bdde
Anton Webb <anton25360@gmail.com>
2020-03-30 12:11:15 +0100

    Added 'get in touch button'

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import './Landing.css'
 
 export class Landing extends Component {
+
+    scroll=(input)=>{
+        document.querySelector(input).scrollIntoView({ 
+            behavior: 'smooth' 
+          });
+        
+    }
+    
     render() {
         return (
             <div className='landingContainer'>
                 <div className='landingTextContainer'>
                     <h1 className='landingTextGreeting'>Hi, I'm Anton.</h1>
                     <p className='landingTextSub'>I'm a Full Stack Web Developer looking for a new challenge.</p><br/><br/>
-                    <button className='landingTextBtn'>View My Work</button>
+                    <button className='landingTextBtn' onClick={()=> this.scroll('.aboutContainer')}>View My Work</button>
+                    <button className='landingTextBtn' onClick={()=> this.scroll('.aboutContainer')}>Get In Touch</button>
                 </div>
             </div>
         )
