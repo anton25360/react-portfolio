4fd4dd27e38fe3b60c8802cafa97d4e56ed77a03
Anton Webb <anton25360@gmail.com>
2020-03-30 17:51:55 +0100

    Added subtle background colors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ export class Projects extends Component {
     render() {
         return (
             <div className='projTarget'>
-            <p id='projTitle'><span id='slash'>/</span>projects</p><br/>
+            <p id='projTitle'><span id='slash'>/</span>projects</p>
             <div className='projectContainer'>
                 <ProjectTemplate title='Wonders' description='7 Wonders of the World PWA.' icon='react'/>
                 <ProjectTemplate title='Wonders' description='7 Wonders of the World PWA.' icon='react'/>
