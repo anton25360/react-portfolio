f2a6c9f1780107cb1ed1b9be05755c88b751fbae
Anton Webb <anton25360@gmail.com>
2020-03-30 12:16:39 +0100

    Added contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Landing.css'
+
+export class Contact extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Contact
