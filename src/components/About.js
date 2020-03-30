69d01cf0250988e7f2b63e2ccdedd8b6e498874e
Anton Webb <anton25360@gmail.com>
2020-03-30 01:14:00 +0100

    smooth scrolls to about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ import gb from '../res/gb.png'
 export class About extends Component {
     render() {
         return (
-            <div className='aboutContainer' id='about'>
+            <div className='aboutContainer'>
                 <p className='aboutTitle'>About Me</p>
                 <p className=' aboutText'>I am a bilingual (<img className='aboutFlag' src={fr} alt='FR'/> & <img className='aboutFlag' src={gb} alt='GB'/>) Full Stack Web Developer from Bath, UK. Thanks to my time at Mayden Academy (as opposed to University) I have experience with many different front and back end languages, and an exeptional eye for well-written code and clean UIs. <br /><br /> I always enjoy putting my skills to the test with various different side-projects (which you can see below), as well as independantly learning new languages (such as Java and Go).<br /><br />When I'm not programming, you'll find me walking my dog, having fun with my CrossFit team mates, tinkering with one of my many Raspberry Pi's, or reading a good Fantasy novel.</p>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import './Navbar.css'
 
 export class Navbar extends Component {
+
+    scrollToAbout=()=>{
+        document.querySelector('.aboutContainer').scrollIntoView({ 
+            behavior: 'smooth' 
+          });
+        
+    }
+
     render() {
+
+
+
         return (
             <div className='navBackground'>
                 <nav>
                     <p className='navElement navLeft'>Anton Webb.</p>
-                    <a className='navElement navRight' href="#contact">Contact</a>
-                    <a className='navElement navRight' href="#projects">Projects</a>
-                    <a className='navElement navRight' href="#about">About</a>
+                    <a className='navElement navRight' href='#contact' onClick={()=> this.scrollToAbout()}>Contact</a>
+                    <a className='navElement navRight' href='#projects' onClick={()=> this.scrollToAbout()}>Projects</a>
+                    <a className='navElement navRight' href="#about" onClick={()=> this.scrollToAbout()}>About</a>
 
                 </nav>
                 
