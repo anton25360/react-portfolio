4949c33ea932db17e8eca316d728fff35c689510
Anton Webb <anton25360@gmail.com>
2020-03-30 18:11:00 +0100

    Card backgrounds are now pure white

--- src/components/ProjectTemplate.js
+++ src/components/ProjectTemplate.js
@@ -8,14 +8,12 @@ export class ProjectTemplate extends Component {
     render() {
         return (
             <div className='PTcontainer'>
-                <img className='PTimage' src='https://picsum.photos/350'/><br/>
+                <img className='PTimage' src='https://picsum.photos/350' /><br />
                 <div className='PTtext'>
-                    <p className='PTtitle'>{this.props.title}</p><br/>
+                    <p className='PTtitle'>{this.props.title}</p><br />
                     <p className='PTdesc'>{this.props.description}</p>
-                    {/* {this.props.name}<br/>{this.props.description} */}
                 </div>
-                {/* <p className='PTDesc'>{this.props.description}</p> */}
-                <img className='PTicon' src={react}/>
+                <img className='PTicon' src={react} />
             </div>
         )
     }
