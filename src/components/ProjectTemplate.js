d7e632abf67679b712cc45adb8d454fc181272f7
Anton Webb <anton25360@gmail.com>
2020-04-01 15:35:28 +0100

    App screenshots are now passed as props

--- src/components/ProjectTemplate.js
+++ src/components/ProjectTemplate.js
@@ -1,60 +1,13 @@
 import React, { Component } from 'react'
 import './ProjectTemplate.css'
 
-//app screenshots
-import freddo from '../res/freddo.png'
-import cascade from '../res/cascade.png'
-import glass from '../res/glass.png'
-import wonders from '../res/wonders.png'
-
-import synergy from '../res/synergy.png'
-import coin from '../res/coin.png'
-import paint from '../res/paint.png'
-import auth from '../res/auth.png'
-import planet from '../res/planet.png'
-import marble from '../res/marble.png'
-import medal from '../res/medal.png'
-import bread from '../res/bread.png'
-
 
 export class ProjectTemplate extends Component {
-
-    selectImage = (input) => {
-        switch (input) {
-            case 'Crypto to Freddo':
-                return freddo;
-            case 'Cascade':
-                return cascade;
-            case 'Glass':
-                return glass;
-            case 'Wonders':
-                return wonders;
-
-            case 'SynergyOS':
-                return synergy;
-            case 'Coin':
-                return coin;
-            case 'Cobra Paint':
-                return paint;
-            case 'Auth':
-                return auth;
-            case 'Planets of the Solar System':
-                return planet;
-            case 'Marble':
-                return marble;
-            case 'Medal Collection':
-                return medal;
-            case 'Bread and Ifits':
-                return bread;
-            default:
-                return coin;
-        }
-    }
 
     render() {
         return (
             <div className='PTcontainer'>
-                <img className='PTimage' src={this.selectImage(this.props.title)} alt='app showcase' /><br />
+                <img className='PTimage' src={this.props.img} alt='app showcase' /><br />
                 <div className='PTtext'>
                     <p className='PTtitle'>{this.props.title}</p>
                     <p className='PTdesc'>{this.props.description}</p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,21 @@ import './Projects.css'
 
 import ProjectTemplate from './ProjectTemplate'
 
+//app screenshots
+import freddo from '../res/freddo.png'
+import cascade from '../res/cascade.png'
+import glass from '../res/glass.png'
+import wonders from '../res/wonders.png'
+
+import synergy from '../res/synergy.png'
+import coin from '../res/coin.png'
+import paint from '../res/paint.png'
+import auth from '../res/auth.png'
+import planet from '../res/planet.png'
+import marble from '../res/marble.png'
+import medal from '../res/medal.png'
+import bread from '../res/bread.png'
+
 export class Projects extends Component {
     render() {
         return (
@@ -10,12 +25,12 @@ export class Projects extends Component {
             <p id='projTitle'><span id='slash'>/</span>projects</p>
             <div className='projectContainer'>
                 {/* react */}
-                <ProjectTemplate title='SynergyOS' description='Browser-based OS (with apps and windows).' tag='ReactJS' class='badge-react'/>
-                <ProjectTemplate title='Wonders' description='7 Wonders of the World travel guide.' tag='ReactJS' class='badge-react'/>
-                <ProjectTemplate title='Crypto to Freddo' description='Cryptocurrency to Freddo converter.' tag='ReactJS' class='badge-react'/>
+                <ProjectTemplate title='SynergyOS' description='Browser-based OS (with apps and windows).' tag='ReactJS' class='badge-react' img={synergy}/>
+                <ProjectTemplate title='Wonders' description='7 Wonders of the World travel guide.' tag='ReactJS' class='badge-react' img={wonders}/>
+                <ProjectTemplate title='Crypto to Freddo' description='Cryptocurrency to Freddo converter.' tag='ReactJS' class='badge-react' img={freddo}/>
 
                 {/* java */}
-                <ProjectTemplate title='Cascade' description='Android To-Do App (with Firebase).' tag='Java' class='badge-java'/>
+                <ProjectTemplate title='Cascade' description='Android To-Do App (with Firebase).' tag='Java' class='badge-java' img={cascade}/>
 
                 {/* go */}
                 {/* cli podcast downloader */}
@@ -24,18 +39,18 @@ export class Projects extends Component {
                 {/* covid19 LED tracker */}
 
                 {/* ts */}
-                <ProjectTemplate title='Glass' description='Cocktail guide (gets data from API).' tag='TypeScript' class='badge-ts'/>
+                <ProjectTemplate title='Glass' description='Cocktail guide (gets data from API).' tag='TypeScript' class='badge-ts' img={glass}/>
 
                 {/* js */}
-                <ProjectTemplate title='Cobra Paint' description='Microsoft Paint Clone.' tag='JavaScript' class='badge-js'/>
-                <ProjectTemplate title='Auth' description='Firebase authentication service.' tag='JavaScript' class='badge-js'/>
-                <ProjectTemplate title='Planets of the Solar System' description='SVG solar system diagram.' tag='JavaScript' class='badge-js'/>
-                <ProjectTemplate title='Marble' description='Gradient text previewer.' tag='JavaScript' class='badge-js'/>
-                <ProjectTemplate title='Coin' description='Simple heads or tails web app.' tag='JavaScript' class='badge-js'/>
+                <ProjectTemplate title='Cobra Paint' description='Microsoft Paint Clone.' tag='JavaScript' class='badge-js' img={paint}/>
+                <ProjectTemplate title='Auth' description='Firebase authentication service.' tag='JavaScript' class='badge-js' img={auth}/>
+                <ProjectTemplate title='Planets of the Solar System' description='SVG solar system diagram.' tag='JavaScript' class='badge-js' img={planet}/>
+                <ProjectTemplate title='Marble' description='Gradient text previewer.' tag='JavaScript' class='badge-js' img={marble}/>
+                <ProjectTemplate title='Coin' description='Simple heads or tails web app.' tag='JavaScript' class='badge-js' img={coin}/>
                 
                 {/* php */}
-                <ProjectTemplate title='Medal Collection' description='Simple MySQL medal collection.' tag='PHP' class='badge-php'/>
-                <ProjectTemplate title='Bread and Ifits' description='Recipe finder (based on available ingredients).' tag='PHP' class='badge-php'/>
+                <ProjectTemplate title='Medal Collection' description='Simple MySQL medal collection.' tag='PHP' class='badge-php' img={medal}/>
+                <ProjectTemplate title='Bread and Ifits' description='Recipe finder (based on available ingredients).' tag='PHP' class='badge-php' img={bread}/>
                 {/* home dashboard */}
 
             </div>
