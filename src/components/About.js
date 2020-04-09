bf41bb0eea53b41515367b21aa594c033674ef37
Anton Webb <anton25360@gmail.com>
2020-04-09 12:35:05 +0100

    reformatted and changed info in about me section

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,13 @@ export class About extends Component {
         return (
             <div className='aboutContainer'>
                 <p className='aboutTitle'><span id='slash'>/</span>about me</p>
-                <p className=' aboutText'>I am a bilingual <img className='aboutFlag' src={fr} alt='FR'/> <img className='aboutFlag' src={gb} alt='GB'/> Full Stack Web Developer from Bath, UK. Thanks to my time at Mayden Academy (as opposed to University) I have experience with many different front and back end languages, and an exeptional eye for well-written code and clean UIs. <br /><br /> I always enjoy putting my skills to the test with various different side projects (which you can see below), as well as independantly learning new languages such as Java and VueJS.<br /><br />When I'm not programming, you'll find me walking my dog, having fun with my CrossFit team mates, tinkering with one of my many Raspberry Pi's, or reading a good Fantasy novel.</p>
+                <p className=' aboutText'>
+                    I am a bilingual <img className='aboutFlag' src={fr} alt='FR'/> <img className='aboutFlag' src={gb} alt='GB'/> Full Stack Web Developer from Bath, UK.
+                    I have experience with various front and back end languages, such as Node, React, and Java, and an exceptional eye for well-written code and clean UIs.<br/><br/>
+                    I studied software development at Mayden Academy, and I enjoy learning new languages and frameworks independently, 
+                    such as Go and VueJS, as well as putting my skills to the test with side projects (which you can see below) .<br/><br/>
+                    When I'm not programming, you'll find me walking my dog, having fun with my CrossFit team mates, 
+                    tinkering with one of many Raspberry Pi's, or reading a good fantasy novel.</p>
             </div>
         )
     }
