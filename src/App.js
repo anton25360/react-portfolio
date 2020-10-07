4050aa668ad9ba4708a995273919e98dfc42e7a3
Anton Webb <anton25360@gmail.com>
2020-10-07 16:24:16 +0100

    Added routes for 2 new components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 //components
-import Navbar from './components/Navbar'
-import Landing from './components/Landing'
-import About from './components/About'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+import Home from './components/pages/Home'
+import Gallery from './components/pages/Gallery'
 
 function App() {
   return (
-    <div >
-      <Navbar />
-      <Landing />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      
+    // <div>
+    //   <Navbar />
+    //   <Landing />
+    //   <About />
+    //   <Skills />
+    //   <Projects />
+    //   <Contact />
+    // </div>
+
+    <Router>
+    <div>
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav> */}
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/gallery">
+          <Gallery />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import "./App.css";
+import '../../App.css'
+
+//components
+import Navbar from "../Navbar";
+import Landing from "../Landing";
+import About from "../About";
+import Skills from "../Skills";
+import Projects from "../Projects";
+import Contact from "../Contact";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Landing />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+
+  
+  );
+}
+
+export default Home;
