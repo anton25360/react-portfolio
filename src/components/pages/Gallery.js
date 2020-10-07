8acbd016140770c6618ee17039fe1e35105f1860
Anton Webb <anton25360@gmail.com>
2020-10-07 16:27:26 +0100

    Added Gallery.css

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../../App.css";
+import './Gallery.css'
 
 function Gallery() {
   return (
     <div>
-      <p>testing</p>
+      <p>Welcome to prohjects gallery</p>
     </div>
   );
 }
