da8ced260528055a5caa53c7a651bde6cf520a87
Anton Webb <anton25360@gmail.com>
2020-10-07 17:09:42 +0100

    styled title

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -5,8 +5,8 @@ import './Gallery.css'
 function Gallery() {
   return (
     <div>
-      <h1>Projects Gallery</h1>
-      <p>The Projects Gallery is meant to showcase all my smaller projects that aren't massivly complex but are still worthy of being part of my portfolio, as well as my laguage diversity.</p>
+      <div className='titleContainer'><h1>Projects Gallery</h1></div>
+      <p>The Projects Gallery is meant to showcase all my smaller projects that aren't massivly complex but are still worthy of being part of my portfolio, as well as my language diversity.</p>
     </div>
   );
 }
