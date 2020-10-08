41cc90b26574d50188daaad979d61039ea19e647
Anton Webb <anton25360@gmail.com>
2020-10-08 13:47:29 +0100

    Added GlassNative + made responsive

--- src/components/MajorProjects.js
+++ src/components/MajorProjects.js
@@ -21,6 +21,12 @@ export default class MajorProjects extends Component {
           storage, account management, remote crashing reports, usage
           statistics, A/B Testing, and more."
         />
+        <MajorProjectTemplate
+          name="Glass"
+          image={glass}
+          gpLink="https://play.google.com/store/apps/details?id=anton25360.github.com.cascade2"
+          info="My first fully fledged React Native app. Glass uses an API to get data of a user-requested cocktail, and displays the ingredients, instructions, and an image in a  beautiful and intuitive UI. Cocktails can also be added to Favourites so they can be quickly previewed from the Favourites Tab. Uses AsyncStorage for persistent data storage and React state for fast and instant loading & previewing. Supports system Dark Mode."
+        />
       </div>
     );
   }
