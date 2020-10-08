b58a7e9ee2b38d5899ec2143e9b77dd0367254aa
Anton Webb <anton25360@gmail.com>
2020-10-08 09:39:31 +0100

    Renamed some IDs

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -4,9 +4,9 @@ import './Gallery.css'
 
 function Gallery() {
   return (
-    <div>
-      <div className='titleContainer'><h1>Projects Gallery</h1></div>
-      <p>The Projects Gallery is meant to showcase all my smaller projects that aren't massivly complex but are still worthy of being part of my portfolio, as well as my language diversity.</p>
+    <div id='galleryContent'>
+      <div id='galleryTitleContainer'><h1 id='galleryTitle'>Projects Gallery</h1></div>
+      <p id='gallerySubtitle'>The Projects Gallery is meant to showcase all my smaller projects that aren't massivly complex but are still worthy of being part of my portfolio, as well as my language diversity.</p>
     </div>
   );
 }
