ef26da3729de0aa1c905fa68fb33ec515974d3af
Anton Webb <anton25360@gmail.com>
2020-10-08 16:10:43 +0100

    tweaked about me section

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export class About extends Component {
             <div className='aboutContainer'>
                 <div id='aboutTitleContainer'><h1 className='aboutTitle'>About Me</h1></div>
                 <p className=' aboutText'>
-                    I am a bilingual <img className='aboutFlag' src={fr} alt='FR'/> <img className='aboutFlag' src={gb} alt='GB'/> Full Stack Web & Mobile Developer from Bath, UK.
+                    I am a Full Stack Web & Mobile Developer from Bath, UK.
                     I have experience with various front and back end languages, such as Node, React, React Native, and Java, and an exceptional eye for well-written code and clean UIs.<br/><br/>
                     I studied software development at Mayden Academy (<a id='aboutLink' href='https://mayden.academy/mayden-academy-ranked-in-the-top-10-best-coding-bootcamps-in-the-world/'>Ranked #6 Best Coding Bootcamp in the world</a>), and I enjoy learning new languages and frameworks independently, 
                     such as Go and VueJS, as well as putting my skills to the test with various projects .<br/><br/>
