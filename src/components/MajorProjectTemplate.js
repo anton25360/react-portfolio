ec254d1e3a644fbd68404a7515db86056e887db3
Anton Webb <anton25360@gmail.com>
2020-10-08 13:23:46 +0100

    removed borders, added placeholder content

--- src/components/MajorProjectTemplate.js
+++ src/components/MajorProjectTemplate.js
@@ -13,13 +13,17 @@ export default class MajorProjectTemplate extends Component {
         <div id="MPTTextContainer">
           <h1 id="MPTTitle">Cascade</h1>
           <p id="MPTInfo">
-            In publishing and graphic design, Lorem ipsum is a placeholder text
-            commonly used to demonstrate the visual form of a document or a
-            typeface without relying on meaningful content.
+            Cascade was my first foray into software development, and one of my
+            most treasured projects to date. The app uses notifications,
+            cloud-based reminders (Cloud Firestore), & account creation for
+            cross-platform saving (Handled by Firebase). Google's developer
+            tools (Firebase) have been fully implemented, allowing for database
+            storage, account management, remote crashing reports, usage
+            statistics, A/B Testing, and more.
           </p>
-          <button id="MPTButton">click me</button>
-          <a href="https://play.google.com/store/apps/details?id=anton25360.github.com.cascade2&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+          <a href="https://play.google.com/store/apps/details?id=anton25360.github.com.cascade2">
             <img
+              id="MPTButton"
               alt="Get it on Google Play"
               src="https://play.google.com/intl/en_gb/badges/static/images/badges/en_badge_web_generic.png"
             />
