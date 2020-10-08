72aaf0c34ffe3be2d5329271d3f50df3ef9deeb8
Anton Webb <anton25360@gmail.com>
2020-10-08 13:34:02 +0100

    added props to MPTemplate

--- src/components/MajorProjectTemplate.js
+++ src/components/MajorProjectTemplate.js
@@ -1,27 +1,17 @@
 import React, { Component } from "react";
 import "./MajorProjectTemplate.css";
 
-import glass from "../res/hd/glass.png";
-import cascade from "../res/hd/cascade.png";
-
 export default class MajorProjectTemplate extends Component {
   render() {
     return (
       <div id="MPTContainer">
-        {/* {this.props.namee} */}
-        <img id="MPTImage" src={cascade} />
+        <img id="MPTImage" src={this.props.image} />
         <div id="MPTTextContainer">
-          <h1 id="MPTTitle">Cascade</h1>
-          <p id="MPTInfo">
-            Cascade was my first foray into software development, and one of my
-            most treasured projects to date. The app uses notifications,
-            cloud-based reminders (Cloud Firestore), & account creation for
-            cross-platform saving (Handled by Firebase). Google's developer
-            tools (Firebase) have been fully implemented, allowing for database
-            storage, account management, remote crashing reports, usage
-            statistics, A/B Testing, and more.
-          </p>
-          <a href="https://play.google.com/store/apps/details?id=anton25360.github.com.cascade2">
+          <h1 id="MPTTitle" className={this.props.name}>
+            {this.props.name}
+          </h1>
+          <p id="MPTInfo">{this.props.info}</p>
+          <a href={this.props.gpLink}>
             <img
               id="MPTButton"
               alt="Get it on Google Play"
--- src/components/MajorProjects.js
+++ src/components/MajorProjects.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import MajorProjectTemplate from "./MajorProjectTemplate";
 import "./MajorProjects.css";
 
+import glass from "../res/hd/glass.png";
+import cascade from "../res/hd/cascade.png";
 
 export default class MajorProjects extends Component {
   render() {
     return (
-      <div id='MPContainer'>
-        <MajorProjectTemplate namee="bro"></MajorProjectTemplate>
+      <div id="MPContainer">
+        <MajorProjectTemplate
+          name="Cascade"
+          image={cascade}
+          gpLink="https://play.google.com/store/apps/details?id=anton25360.github.com.cascade2"
+          info="Cascade was my first foray into software development, and one of my
+          most treasured projects to date. The app uses notifications,
+          cloud-based reminders (Cloud Firestore), & account creation for
+          cross-platform saving (Handled by Firebase). Google's developer
+          tools (Firebase) have been fully implemented, allowing for database
+          storage, account management, remote crashing reports, usage
+          statistics, A/B Testing, and more."
+        />
       </div>
     );
   }
