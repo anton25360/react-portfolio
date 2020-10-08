f131520b6d9b40c7308ed75eb7de99ee569a55d4
Anton Webb <anton25360@gmail.com>
2020-10-08 16:37:36 +0100

    Updated Routing paths

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ import Gallery from "./components/pages/Gallery";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/gallery">
-            <Gallery />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //     <Switch>
+    //       <Route exact path="/">
+    //         <Home />
+    //       </Route>
+    //       <Route exact path="/gallery">
+    //         <Gallery />
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+
+<Router basename="/build">
+<Switch>
+    {/* <Route path="/cafes/:cafe" component={ Cafe } />
+    <Route path="/cafes" component={ CafeList } />
+    <Route path="/add" component={ AddCafeForm } /> */}
+    <Route path="/gallery" component={ Gallery } />
+    <Route path="/" component={ Home } />
+    {/* <Route component={ NotFound } /> */}
+</Switch>
+</Router>   
   );
 }
 
